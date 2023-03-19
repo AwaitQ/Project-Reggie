4992b7adc045702bf09dd86a98b94c785b2dfd2b
AwaitQ <739639036@qq.com>
2023-03-19 19:16:14 +0800

    测试dev

--- src/main/java/com/lv/reggie/controller/testController.java
+++ src/main/java/com/lv/reggie/controller/testController.java
@@ -3,5 +3,6 @@ package com.lv.reggie.controller;
 public class testController {
     public static void main(String[] args) {
         System.out.println("test111");
+        System.out.println("test dev01");
     }
 }
