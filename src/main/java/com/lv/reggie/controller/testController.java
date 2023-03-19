5ca8d43cf0c9ccf29f55205045e22ca26590294b
AwaitQ <739639036@qq.com>
2023-03-19 19:47:43 +0800

    dev第二次修改

--- src/main/java/com/lv/reggie/controller/testController.java
+++ src/main/java/com/lv/reggie/controller/testController.java
@@ -3,7 +3,8 @@ package com.lv.reggie.controller;
 public class testController {
     public static void main(String[] args) {
         System.out.println("test111");
-        System.out.println("test dev01");
+        System.out.println("test dev001");
         System.out.println("test remote02");
+        System.out.println("test dev-002");
     }
 }
